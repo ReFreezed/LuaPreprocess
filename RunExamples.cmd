af7bcb08a0487cbef8d2cff3b68ce3e7c87a8a5b
ReFreezed <ReFreezed@users.noreply.github.com>
2019-01-05 22:09:25 +0100

    Improved RunExamples.cmd

--- RunExamples.cmd
+++ RunExamples.cmd
@@ -1,28 +1,9 @@
 @ECHO OFF
 
-SET example=namedConstants
-ECHO.
-ECHO Running %example%...
-lua main.lua examples\%example%.lua2p --debug --silent
-lua          examples\%example%.lua
+FOR /R examples %%G IN (*.lua2p) DO (
+	ECHO. & ECHO Processing example '%%~nxG'...
+	lua main.lua "%%G" --debug --silent
+	lua "%%~dpnG.lua"
+)
 
-SET example=optimizeDataAccess
-ECHO.
-ECHO Running %example%...
-lua main.lua examples\%example%.lua2p --debug --silent
-lua          examples\%example%.lua
-
-SET example=parseFile
-ECHO.
-ECHO Running %example%...
-lua main.lua examples\%example%.lua2p --debug --silent
-lua          examples\%example%.lua
-
-SET example=selectiveFunctionality
-ECHO.
-ECHO Running %example%...
-lua main.lua examples\%example%.lua2p --debug --silent
-lua          examples\%example%.lua
-
-ECHO.
-ECHO All examples finished!
+ECHO. & ECHO All examples finished!
