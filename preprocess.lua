25492219fca2558e3a5f642131881bba22dbcd15
ReFreezed <ReFreezed@users.noreply.github.com>
2022-06-20 13:37:48 +0200

    Updated changelog.

--- preprocess.lua
+++ preprocess.lua
@@ -1,6 +1,6 @@
 --[[============================================================
 --=
---=  LuaPreprocess v1.18-dev - preprocessing library
+--=  LuaPreprocess v1.19 - preprocessing library
 --=  by Marcus 'ReFreezed' Thunström
 --=
 --=  License: MIT (see the bottom of this file)
@@ -131,7 +131,7 @@
 
 
 
-local PP_VERSION = "1.18.0-dev"
+local PP_VERSION = "1.19.0"
 
 local MAX_DUPLICATE_FILE_INSERTS = 1000 -- @Incomplete: Make this a parameter for processFile()/processString().
 
